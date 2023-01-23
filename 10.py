a9a511f5f8d1b7a73b7a51e6198ec28786e52bb4
mrldantas <123373325+mrldantas@users.noreply.github.com>
2023-01-23 08:54:15 -0300

    Update 10.py

--- 10.py
+++ 10.py
@@ -1,3 +1,5 @@
+# Faça um Programa que peça a temperatura em graus Celsius, transforme e mostre em graus Fahrenheit.
+
 Celsius = int(input("Insira a temperatura em celsius: "))
 
 resultado = round((Celsius * 1.8) + 32)
