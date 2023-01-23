bc978e4a1128d1b0ed4bae0e4efa763240f806b9
mrldantas <123373325+mrldantas@users.noreply.github.com>
2023-01-23 08:55:48 -0300

    Update 13.py

--- 13.py
+++ 13.py
@@ -1,3 +1,7 @@
+# Tendo como dado de entrada a altura (h) de uma pessoa, construa um algoritmo que calcule seu peso ideal, utilizando as seguintes fórmulas:
+# Para homens: (72.7*h) - 58
+# Para mulheres: (62.1*h) - 44.7
+
 sexo = input("Insira se você é homem ou mulher: ")
 
 if (sexo == "Homem" or sexo == "homem"):
