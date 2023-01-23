a57479bab79c49bcb511dce61462cc48201a560b
mrldantas <123373325+mrldantas@users.noreply.github.com>
2023-01-23 08:52:45 -0300

    Update 06.py

--- 06.py
+++ 06.py
@@ -1,3 +1,5 @@
+# Faça um Programa que peça o raio de um círculo, calcule e mostre sua área.
+
 raioCirculo = float(input("Insira o raio do circulo: "))
 
 print(f"A área é: {3.14+(raioCirculo * raioCirculo)}")
