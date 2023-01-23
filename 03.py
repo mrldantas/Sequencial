7f54b887a8d5a31685bd56598cfef254b426d507
mrldantas <123373325+mrldantas@users.noreply.github.com>
2023-01-23 08:51:29 -0300

    Update 03.py

--- 03.py
+++ 03.py
@@ -1,3 +1,5 @@
+# Faça um Programa que peça dois números e imprima a soma.
+
 primeiroNumero = int(input("Insira o primeiro número da soma: "))
 segundoNumero = int(input("Insira o segundo número: "))
 
