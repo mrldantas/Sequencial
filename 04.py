965a603eb6c9006e05588d3e0bcdf0595d6dc293
mrldantas <123373325+mrldantas@users.noreply.github.com>
2023-01-23 08:51:56 -0300

    Update 04.py

--- 04.py
+++ 04.py
@@ -1,3 +1,5 @@
+# Faça um Programa que peça as 4 notas bimestrais e mostre a média.
+
 primeiraNota = float(input("Insira a nota do primeiro bimestre: "))
 segundaNota = float(input("Insira a nota do segundo bimestre: "))
 terceiraNota = float(input("Insira a nota do terceiro bimestre: "))
