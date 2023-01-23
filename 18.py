ad0882a5bbf5583d30f9b7e481a2fc81939369e0
mrldantas <123373325+mrldantas@users.noreply.github.com>
2023-01-23 09:01:35 -0300

    Update 18.py

--- 18.py
+++ 18.py
@@ -1,3 +1,6 @@
+# Faça um programa que peça o tamanho de um arquivo para download (em MB) e a velocidade de um link de 
+# Internet (em Mbps), calcule e informe o tempo aproximado de download do arquivo usando este link (em minutos).
+
 tamanhoArquivo = float(input("Insira o tamanho do arquivo em MB: "))
 velocidadeInternet = int(input("Insira a velocidade da sua internet: "))
 
